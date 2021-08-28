aba2c96d39c2c5bc27a373027425199924257b36
Daniel Sieradski <daniel@self.agency>
2021-08-28 14:30:21 -0400

    vercel wip

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import * as Sentry from '@sentry/node'
+import Sentry from '@sentry/node'
 import { VercelRequest, VercelResponse } from '@vercel/node'
 import Fastify, { FastifyInstance } from 'fastify'
 import FastifySentry from 'fastify-sentry'
 import app from './app'
 
-const server: FastifyInstance = Fastify({
+const fastify: FastifyInstance = Fastify({
   logger: true
 })
 
 if (process.env.SENTRY_DSN) {
-  server.register(FastifySentry, {
+  fastify.register(FastifySentry, {
     dsn: process.env.SENTRY_DSN,
     environment: process.env.ENVIRONMENT,
     integrations: [new Sentry.Integrations.Http({ tracing: true })],
@@ -17,9 +17,9 @@ if (process.env.SENTRY_DSN) {
   })
 }
 
-server.register(app)
+fastify.register(app)
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  await server.ready()
-  return server.server.emit('request', req, res)
+  await fastify.ready()
+  return fastify.server.emit('request', req, res)
 }
